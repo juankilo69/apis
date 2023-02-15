50a3af1fde5f1ff91abe147b8854e52f76dec986
unknown <juankilop69@gmail.com>
2023-02-15 19:48:10 +0100

    si

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let peticion = new XMLHttpRequest()                 //Creamos el objeto para la peticion
+let peticion = new XMLHttpRequest()
+let peticion2 = new XMLHttpRequest()                
 let urlM = "http://gateway.marvel.com/v1/public/creators/32/comics?ts=1000&apikey=fe2b6d8979489384544444da46b59af5&hash=77a33bf1cb7b74086ff8e2ea8cb4a72a"
 let urlP = "https://pokeapi.co/api/v2/pokemon"
 let urlS = ""
@@ -16,7 +17,7 @@ let pintadoS = false
 
 
 document.querySelector("button").addEventListener("click",()=>{
-    console.log(change.value);
+    //console.log(change.value);
     if(change.value == "marvel"){
         divP.style.display = "none"
         divS.style.display = "none"
@@ -48,36 +49,56 @@ document.querySelector("button").addEventListener("click",()=>{
     }else if(change.value == "pokemon"){
         divM.style.display = "none"
         divS.style.display = "none"
-    
-        peticion.onreadystatechange = function() {
-            if(pintadoP != true){
-                peticion.open("GET", urlP,true)      
-                peticion.send(null)
-                peticion.onreadystatechange = function() {
-                    if(this.readyState == 4 && this.status == 200){          
-                        let datos = JSON.parse(this.responseText)           
-                        console.log(datos.results);
-                        for(let d of datos.results){                              
-    
-                            divP.innerHTML += `<div class="objeto">
-                                <h1>${d.name}</h1>
-                            </div>`                                                                 
+
+        if(pintadoP != true){
+            peticion.open("GET", urlP,true)      
+            peticion.send(null)
+            peticion.onreadystatechange = function() {
+                if(this.readyState == 4 && this.status == 200){          
+                    let datos = JSON.parse(this.responseText)           
+                    console.log(datos.results);
+                    for(p of datos.results){                              
+                        let urlPokemon = []
+                        urlPokemon.push(p.url)
+                        console.log(urlPokemon);
+                        let habilidades
+                        let exp
+                        for(let u = 0; u<urlPokemon.length;u++){
+                            console.log("hola");
+                            peticion2.open("GET", urlPokemon[u],true)      
+                            peticion2.send(null)
+                            
+                            peticion2.onreadystatechange = function() {
+                                if(this.readyState == 4 && this.status == 200){         
+                                    let datos2 = []
+                                    datos2.push(this.responseText) 
+                                    
+                                    exp = datos2.height       
+                                }
+                                console.log("2222",exp);
+                            }
                         }
+                        divP.innerHTML += `<div class="objeto">
+                            <p>${p.name}</p>
+                            <p>${exp}</p>
+                        </div>`
                     }
                 }
-                pintadoP = true
-            }else{
-                divP.style.display = "flex"
             }
+            pintadoP = true
+        }else{
+            divP.style.display = "flex"
         }
         
     }else if(change.value == "starwars"){
         divM.style.display = "none"
         divP.style.display = "none"
+
+        peticion.open("GET", urlS,true)      
+        peticion.send(null)
+
         peticion.onreadystatechange = function() {
             if(pintadoS != true){
-                peticion.open("GET", urlS,true)      
-                peticion.send(null)
                 peticion.onreadystatechange = function() {
                     if(this.readyState == 4 && this.status == 200){          
                         let datos = JSON.parse(this.responseText)           
